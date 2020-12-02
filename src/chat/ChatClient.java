4a12f2e53426c844f225cf240e4561d76bc5b29e
eunho <eunho3854@gmail.com>
2020-12-02 16:08:44 +0900

    자바 소켓 채팅 시험

--- src/chat/ChatClient.java
+++ src/chat/ChatClient.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -109,7 +111,14 @@ public class ChatClient extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				send();
+			}
+		});
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
 				text();
+				System.exit(0);
 			}
 		});
 	}
